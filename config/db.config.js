d8fdc689a62178abe594cbac64087985fd4b40b9
Jordan MOYO <lordmoyojordan@gmail.com>
2020-03-30 02:08:43 +0100

    initial commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,9 @@
+// connection parameters to cleardb
 module.exports = {
-    HOST       : "localhost",
-    USER       : "root",
-    PASSWORD   : "password",
-    DB         : "nodejsVueMysqlApp",
+    HOST       : "eu-cdbr-west-02.cleardb.net",
+    USER       : "b0bd0280d926ca",
+    PASSWORD   : "f15d6787",
+    DB         : "heroku_33682a2b2e0ab67",
     dialect    : "mysql",
     pool       : {
         max    : 5,
@@ -11,3 +12,17 @@ module.exports = {
         idle   : 10000
     }
 };
+
+//module.exports = {
+    //     HOST       : "localhost",
+    //     USER       : "root",
+    //     PASSWORD   : "password",
+    //     DB         : "nodejsVueMysqlApp",
+    //     dialect    : "mysql",
+    //     pool       : {
+    //         max    : 5,
+    //         min    : 0,
+    //         acquire: 30000,
+    //         idle   : 10000
+    //     }
+    // };
