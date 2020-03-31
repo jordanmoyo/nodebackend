ff9aff4c27a093c65cb0d687d36da331169a3602
Jordan MOYO <lordmoyojordan@gmail.com>
2020-03-31 20:16:30 +0100

    test

--- client/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL : "https://tutorial-node-vue-mysql.herokuapp.com/api" || "http://localhost:3000/api",
-    headers : {
-        "Content-type"  : "application/json"
-    }
-});
--- server.js
+++ server.js
@@ -41,13 +41,13 @@ require("./app/routes/tutorial.routes")(app);
 const PORT = process.env.PORT || 3000;
 
 
-if( process.env.NODE_ENV === "production" ){
-    app.use(express.static('client/dist'));
+// if( process.env.NODE_ENV === "production" ){
+//     app.use(express.static('client/dist'));
 
-    app.get('*', (req, res) =>{
-        res.sendFile(path.join(__dirname, 'client','dist','index.html')); 
-    });
-}
+//     app.get('*', (req, res) =>{
+//         res.sendFile(path.join(__dirname, 'client','dist','index.html')); 
+//     });
+// }
 
 
 app.listen(PORT, () => {
