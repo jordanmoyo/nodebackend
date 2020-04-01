0ba7c74f59845dcac5ad1b1d83439376c83897b9
Jordan MOYO <lordmoyojordan@gmail.com>
2020-04-01 02:47:07 +0100

    test

--- config/db.config.js
+++ config/db.config.js
@@ -1,28 +1,28 @@
 // connection parameters to cleardb
-// module.exports = {
-//     HOST       : "eu-cdbr-west-02.cleardb.net",
-//     USER       : "bb41f4f25964da",
-//     PASSWORD   : "cedf756e",
-//     DB         : "heroku_32a735a0b851266",
-//     dialect    : "mysql",
-//     pool       : {
-//         max    : 5,
-//         min    : 0,
-//         acquire: 30000,
-//         idle   : 10000
-//     }
-// };
-
 module.exports = {
-        HOST       : "eu-cdbr-west-02.cleardb.net"||"localhost",
-        USER       : "bb41f4f25964da"||"root",
-        PASSWORD   : "cedf756e"|| "password",
-        DB         : "heroku_32a735a0b851266"||"nodejsVueMysqlApp",
-        dialect    : "mysql",
-        pool       : {
-            max    : 5,
-            min    : 0,
-            acquire: 30000,
-            idle   : 10000
-        }
-    };
+    HOST       : "remotemysql.com",
+    USER       : "KOq48HunCq",
+    PASSWORD   : "dCv8UZFjgP",
+    DB         : "KOq48HunCq",
+    dialect    : "mysql",
+    pool       : {
+        max    : 5,
+        min    : 0,
+        acquire: 30000,
+        idle   : 10000
+    }
+};
+
+// module.exports = {
+//         HOST       : "eu-cdbr-west-02.cleardb.net"||"localhost",
+//         USER       : "bb41f4f25964da"||"root",
+//         PASSWORD   : "cedf756e"|| "password",
+//         DB         : "heroku_32a735a0b851266"||"nodejsVueMysqlApp",
+//         dialect    : "mysql",
+//         pool       : {
+//             max    : 5,
+//             min    : 0,
+//             acquire: 30000,
+//             idle   : 10000
+//         }
+//     };
